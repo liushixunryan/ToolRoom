6e840e3791f1f50a444d4ce46d29cd8f1cbb2059
Ryanliu <33927708+liushixunryan@users.noreply.github.com>
2021-07-01 08:11:11 +0800

    开始获取真实数据,折线图没开始

--- weather/src/main/java/com/xql/weather/WeatherActivity.java
+++ weather/src/main/java/com/xql/weather/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.xql.weather;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.util.Log;
 
 import androidx.lifecycle.Observer;
@@ -10,22 +11,51 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.blankj.utilcode.util.GsonUtils;
+import com.blankj.utilcode.util.PermissionUtils;
+import com.blankj.utilcode.util.TimeUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.qweather.sdk.bean.IndicesBean;
+import com.qweather.sdk.bean.WarningBean;
+import com.qweather.sdk.bean.air.AirNowBean;
 import com.qweather.sdk.bean.geo.GeoBean;
+import com.qweather.sdk.bean.sunmoon.MoonBean;
+import com.qweather.sdk.bean.sunmoon.SunBean;
+import com.qweather.sdk.bean.weather.WeatherHourlyBean;
+import com.qweather.sdk.bean.weather.WeatherNowBean;
 import com.xql.annotation.BindPath;
 import com.xql.basic.activity.BaseActivity;
+import com.xql.common.LineChartManager;
 import com.xql.weather.databinding.ActivityWeatherBinding;
 import com.xql.weather.vm.WeatherVM;
+import com.yanzhenjie.permission.runtime.Permission;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @BindPath(key = "weather/weather")
 public class WeatherActivity extends BaseActivity<ActivityWeatherBinding, WeatherVM> {
     //定位需要的声明
     private AMapLocationClient mLocationClient = null;//定位发起端
     private AMapLocationClientOption mLocationOption = null;//定位参数
+    //省市名
+    private String city;
     //城市名
     private String loction;
     //和风天气城市id
     private String districtId;
+    //温度List
+    private List<Entry> entries = new ArrayList<>();
 
     @Override
     protected int layoutId() {
@@ -34,34 +64,49 @@ public class WeatherActivity extends BaseActivity<ActivityWeatherBinding, Weathe
 
     @Override
     protected void initView() {
-
+        showLoading();
         /**
          * 高德地图定位服务
          */
         Positioningservice();
+        /**
+         * MpAndroidchart折线图
+         */
+//        MpAndroidchart();
     }
 
+
+    /**
+     * 高德地图初始化配置
+     */
     private void Positioningservice() {
-        //初始化定位
-        mLocationClient = new AMapLocationClient(getApplicationContext());
-        //设置定位回调监听
-        mLocationClient.setLocationListener(mLocationListener);
-        //初始化定位参数
-        mLocationOption = new AMapLocationClientOption();
-        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
-        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        //设置是否返回地址信息（默认返回地址信息）
-        mLocationOption.setNeedAddress(true);
-        //设置是否只定位一次,默认为false
-        mLocationOption.setOnceLocation(false);
-        //设置是否允许模拟位置,默认为false，不允许模拟位置
-        mLocationOption.setMockEnable(false);
-        //设置定位间隔,单位毫秒,默认为2000ms
-        mLocationOption.setInterval(60000);
-        //给定位客户端对象设置定位参数
-        mLocationClient.setLocationOption(mLocationOption);
-        //启动定位
-        mLocationClient.startLocation();
+        boolean granted = PermissionUtils.isGranted(Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION);
+        if (granted) {
+            //初始化定位
+            mLocationClient = new AMapLocationClient(getApplicationContext());
+            //设置定位回调监听
+            mLocationClient.setLocationListener(mLocationListener);
+            //初始化定位参数
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置是否返回地址信息（默认返回地址信息）
+            mLocationOption.setNeedAddress(true);
+            //设置是否只定位一次,默认为false
+            mLocationOption.setOnceLocation(false);
+            //设置是否允许模拟位置,默认为false，不允许模拟位置
+            mLocationOption.setMockEnable(false);
+            //设置定位间隔,单位毫秒,默认为2000ms
+            mLocationOption.setInterval(60000);
+            //给定位客户端对象设置定位参数
+            mLocationClient.setLocationOption(mLocationOption);
+            //启动定位
+            mLocationClient.startLocation();
+        } else {
+            ToastUtils.showLong("您没有授权将无法使用天气功能");
+            showLoading();
+        }
+
     }
 
     //声明定位回调监听器
@@ -71,7 +116,7 @@ public class WeatherActivity extends BaseActivity<ActivityWeatherBinding, Weathe
         public void onLocationChanged(AMapLocation aMapLocation) {
             String s = "纬度" + aMapLocation.getLatitude() + "/经度" + aMapLocation.getLongitude() + "/精度" + aMapLocation.getAccuracy() + "/地址描述" + aMapLocation.getAddress() + "/国家" + aMapLocation.getCountry() + "/省" + aMapLocation.getProvince() + "/城市" + aMapLocation.getCity() + "/城区" + aMapLocation.getDistrict() + "/街道" + aMapLocation.getStreet() + "/街道门牌号" + aMapLocation.getStreetNum();
             loction = aMapLocation.getDistrict();
-            mBinding.cityTitle.setText(aMapLocation.getCity() + aMapLocation.getDistrict());
+            city = aMapLocation.getCity();
             GeoAPI();
         }
     };
@@ -80,13 +125,147 @@ public class WeatherActivity extends BaseActivity<ActivityWeatherBinding, Weathe
      * GEOAPI获取城市id
      */
     private void GeoAPI() {
-        mViewModel.getGeo(WeatherActivity.this, loction).observe(WeatherActivity.this, new Observer<GeoBean>() {
+        mViewModel.getGeo(WeatherActivity.this, "大东区").observe(WeatherActivity.this, new Observer<GeoBean>() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onChanged(GeoBean geoBean) {
-                Log.e(TAG, "onChanged: " + geoBean.getLocationBean().get(0).getId());
+                mBinding.cityTitle.setText(city + loction);
+                Log.e(TAG, "GeoAPI: " + geoBean.getLocationBean().get(0).getId());
                 districtId = geoBean.getLocationBean().get(0).getId();
+                //实况天气
+                WeatherNow();
+                //24小时预报
+                Weather24Hourly();
+                //生活指数
                 Indices1D();
+                //日出日落
+                SunToday();
+                //月升月落月相
+                MoonToday();
+                //空气质量
+                AirNow();
+                //灾害预警
+                Warning();
+                hideLoading();
+            }
+        });
+    }
 
+    /**
+     * 获取24小时预报
+     */
+    private void Weather24Hourly() {
+        mViewModel.getWeather24Hourly(WeatherActivity.this,districtId).observe(WeatherActivity.this, new Observer<WeatherHourlyBean>() {
+            @Override
+            public void onChanged(WeatherHourlyBean weatherHourlyBean) {
+                String s = GsonUtils.toJson(weatherHourlyBean);
+                Log.e(TAG, "24小时预报: " + s);
+                new LineChartManager(mBinding.lineChart, entries);
+                for (int i = 0; i < 24; i++) {
+                    entries.add(new Entry(i, (float) (Math.random()) * 80));
+                }
+            }
+        });
+    }
+
+    /**
+     * 获取灾害预警
+     */
+    private void Warning() {
+        mViewModel.getWarning(WeatherActivity.this,districtId).observe(WeatherActivity.this, new Observer<WarningBean>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onChanged(WarningBean warningBean) {
+                String s = GsonUtils.toJson(warningBean);
+                Log.e(TAG, "灾害预警: " + s);
+                String PubTime = warningBean.getWarningList().get(0).getPubTime().substring(0,10) + " " + warningBean.getWarningList().get(0).getPubTime().substring(11,16);
+                String startTime = warningBean.getWarningList().get(0).getStartTime().substring(0,10) + " " + warningBean.getWarningList().get(0).getPubTime().substring(11,16);
+                String endTime = warningBean.getWarningList().get(0).getEndTime().substring(0,10) + " " + warningBean.getWarningList().get(0).getPubTime().substring(11,16);
+                mBinding.disasterEarlyWarningLayout.pubTimeTv.setText(PubTime);
+                mBinding.disasterEarlyWarningLayout.warnTitleTv.setText(warningBean.getWarningList().get(0).getTitle() + "");
+                mBinding.disasterEarlyWarningLayout.startTimeTv.setText(startTime);
+                mBinding.disasterEarlyWarningLayout.endTimeTv.setText(endTime);
+                mBinding.disasterEarlyWarningLayout.levelTv.setText(warningBean.getWarningList().get(0).getLevel() + "");
+                mBinding.disasterEarlyWarningLayout.typeNameTv.setText(warningBean.getWarningList().get(0).getTypeName() + "");
+                mBinding.disasterEarlyWarningLayout.warnTextTv.setText(warningBean.getWarningList().get(0).getText() + "");
+            }
+        });
+    }
+
+    /**
+     * 获取空气质量
+     */
+    private void AirNow() {
+        mViewModel.getAirNow(WeatherActivity.this, districtId).observe(WeatherActivity.this, new Observer<AirNowBean>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onChanged(AirNowBean airNowBean) {
+                String s = GsonUtils.toJson(airNowBean);
+                Log.e(TAG, "空气质量: " + s);
+                mBinding.airQualityLayout.aqiTv.setText(airNowBean.getNow().getAqi() + "");
+                mBinding.airQualityLayout.primaryTv.setText(airNowBean.getNow().getPrimary() + "");
+                mBinding.airQualityLayout.levelTv.setText(airNowBean.getNow().getLevel() + "");
+                mBinding.airQualityLayout.categoryTv.setText(airNowBean.getNow().getCategory() + "");
+                mBinding.airQualityLayout.pm10Tv.setText(airNowBean.getNow().getPm10() + "");
+                mBinding.airQualityLayout.pm2p5Tv.setText(airNowBean.getNow().getPm2p5() + "");
+                mBinding.airQualityLayout.no2Tv.setText(airNowBean.getNow().getNo2() + "");
+                mBinding.airQualityLayout.so2Tv.setText(airNowBean.getNow().getSo2() + "");
+                mBinding.airQualityLayout.coTv.setText(airNowBean.getNow().getCo() + "");
+                mBinding.airQualityLayout.o3Tv.setText(airNowBean.getNow().getO3() + "");
+            }
+        });
+    }
+
+    /**
+     * 获取月升月落月相
+     */
+    private void MoonToday() {
+        mViewModel.getMoon(WeatherActivity.this, districtId).observe(WeatherActivity.this, new Observer<MoonBean>() {
+            @Override
+            public void onChanged(MoonBean moonBean) {
+                String s = GsonUtils.toJson(moonBean);
+                Log.e(TAG, "月升月落月相: " + s);
+                String Moonrise = moonBean.getMoonrise().substring(11, 16);
+                String Moonset = moonBean.getMoonset().substring(11, 16);
+                mBinding.sunandmoonLayout.moonriseTimeTv.setText(Moonrise);
+                mBinding.sunandmoonLayout.moonsetTimeTv.setText(Moonset);
+                mBinding.sunandmoonLayout.phaseMoonTv.setText(moonBean.getMoonPhaseBeanList().get(0).getName());
+            }
+        });
+    }
+
+    /**
+     * 获取日出日落时间
+     */
+    private void SunToday() {
+        mViewModel.getSun(WeatherActivity.this, districtId).observe(WeatherActivity.this, new Observer<SunBean>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onChanged(SunBean sunBean) {
+                String s = GsonUtils.toJson(sunBean);
+                Log.e(TAG, "日出日落: " + s);
+                String Sunrise = sunBean.getSunrise().substring(11, 16);
+                String Sunset = sunBean.getSunset().substring(11, 16);
+                mBinding.sunandmoonLayout.sunriseTimeTv.setText(Sunrise);
+                mBinding.sunandmoonLayout.sunsetTimeTv.setText(Sunset);
+            }
+        });
+    }
+
+    /**
+     * 获取实况天气
+     */
+    private void WeatherNow() {
+        mViewModel.getWeatherNow(WeatherActivity.this, districtId).observe(WeatherActivity.this, new Observer<WeatherNowBean>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onChanged(WeatherNowBean weatherNowBean) {
+                String s = GsonUtils.toJson(weatherNowBean);
+                Log.e(TAG, "实况天气: " + s);
+                mBinding.airTemperature.temperatureTv.setText(weatherNowBean.getNow().getTemp() + "℃");
+                mBinding.airTemperature.weatherConditionsTv.setText("天气: " + weatherNowBean.getNow().getText());
+                mBinding.airTemperature.windDirectionTv.setText("风向: " + weatherNowBean.getNow().getWindDir());
+                mBinding.airTemperature.windPowerTv.setText("风力: " + weatherNowBean.getNow().getWindScale() + "级");
             }
         });
     }
@@ -99,10 +278,11 @@ public class WeatherActivity extends BaseActivity<ActivityWeatherBinding, Weathe
             @Override
             public void onChanged(IndicesBean indicesBean) {
                 String s = GsonUtils.toJson(indicesBean);
-                Log.e(TAG, "onChanged: " + s);
+                Log.e(TAG, "生活指数: " + s);
             }
         });
     }
+
 
     @Override
     protected void initData() {
