ad6c4d56c2cb4c7b7fe51e5d221c3c37522485e6
Ryanliu <33927708+liushixunryan@users.noreply.github.com>
2021-08-12 17:02:19 +0800

    重写天气模块
    高德地图key改完了,代码中还没配置

---
+++ weather/src/main/java/com/xql/weather/WeatherActivity.java
@@ -0,0 +1,24 @@
+package com.xql.weather;
+
+import com.xql.basic.activity.BaseActivity;
+import com.xql.weather.databinding.ActivityWeatherBinding;
+import com.xql.weather.vm.WeatherVM;
+
+public class WeatherActivity extends BaseActivity<ActivityWeatherBinding, WeatherVM> {
+
+
+    @Override
+    protected int layoutId() {
+        return R.layout.activity_weather;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ weather/src/main/java/com/xql/weather/vm/WeatherVM.java
@@ -0,0 +1,13 @@
+package com.xql.weather.vm;
+
+import com.xql.basic.viewmodel.BaseViewModel;
+
+/**
+ * @ClassName: WeatherVM
+ * @Description: java类作用描述
+ * @CreateDate: 2021/8/12 16:37
+ * @UpdateUser: RyanLiu
+ */
+
+public class WeatherVM extends BaseViewModel {
+}
